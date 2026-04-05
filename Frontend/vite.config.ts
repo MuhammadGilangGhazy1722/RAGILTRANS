66590b2f7ad6116949734e5d63f5a5cc350df009
MuhammadGilangGhazy1722 <lankghzy@gmail.com>
2026-04-05 14:37:26 +0700

    fix: remove devtools from vite config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,20 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [
-    ...(import.meta.env.MODE !== 'production' ? [devtools()] : []),
     solidPlugin(), 
     tailwindcss()
   ],
   server: {
-    port: 5173, // Frontend port
-    host: true, // Allow external access
+    port: 5173,
+    host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Backend port
+        target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
       }
@@ -22,8 +20,6 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
-    minify: 'terser', // Minify for production
+    minify: 'terser',
   },
 });
-
-
